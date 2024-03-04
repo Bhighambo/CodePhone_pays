d4855e55dc95efb699a933845f8768741b514b09
Bhighambo <jacksonkennedy336@gmail.com>
2024-03-04 09:14:29 +0200

    add codes

--- index.php
+++ index.php
@@ -4,8 +4,46 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>La liste des pays</title>
+	<script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<div class="select-box">
+		<div class="selected-option">
+			<div>
+				<span class="iconify" data-icon="flag:gb-4x3"></span>
+				<strong>+44</strong>
+			</div>
+			<input type="tel" name="tel" placeholder="Phone Number">
+		</div>
+		<div class="options">
+			<input type="text" name="" class="search-box" placeholder="Recheche du pays">
+			<ol>
+				<li class="option">
+					<div>
+						<span class="iconify" data-icon="flag:cd-4x3"></span>
+						<span class="county-name">DR Congo</span>
+					</div>
+					<strong>+243</strong>
+				</li>
+				<li class="option">
+					<div>
+						<span class="iconify" data-icon="flag:bg-4x3"></span>
+						<span class="county-name">United Kingdom</span>
+					</div>
+					<strong>+44</strong>
+				</li>
+				<li class="option">
+					<div>
+						<span class="iconify" data-icon="flag:us-4x3"></span>
+						<span class="county-name">United</span>
+					</div>
+					<strong>+1</strong>
+				</li>
+			</ol>
+		</div>
+		
+	</div>
 
 </body>
 </html>
