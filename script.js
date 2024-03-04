25f7c9976bd0ddb3155192c64d8e92f3aca64212
Bhighambo <jacksonkennedy336@gmail.com>
2024-03-04 18:04:11 +0200

    give a good fonctionnality for countries

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 
-const  countries = [
-	{ name: "Afghanistan", code: "AF", phone: 93},
-	{ name: "DR Cong", code: "CD", phone: 243},
-	{ name: "Afghanistan", code: "AF", phone: 93},
-	{ name: "Afghanistan", code: "AF", phone: 93},
-	{ name: "Afghanistan", code: "AF", phone: 93}
-],
+const countries = [
+    { name: "Afghanistan", code: "AF", phone: 93 },
+    { name: "DR Cong", code: "CD", phone: 243 },
+    { name: "Zimbabwe", code: "ZW", phone: 263 },
+    { name: "Zambi", code: "ZM", phone: 260 },
+    { name: "Uganda", code: "UG", phone: 225 },
+    { name: "Yemen", code: "YE", phone: 967 }
+];
 
-for (country of countries){
-	const option = `
-		<li class="option">
-			<div>
-				<span class="iconify" data-icon="flag:${country.code.toLowerCase()}-4x3"></span>
-				<span class="county-name">${country.name}</span>
-			</div>
-		    <strong>+${country.phone}</strong>
-		</li>
-	`;
+// Assurez-vous que select_box est correctement défini
+const select_box = document.querySelector('.options'),
+    search_box = document.querySelector('.search-box'),
+    input_box = document.querySelector('input[type="tel"]'),
+    selected_option = document.querySelector('.selected-option div');
 
-	select_box.querySelector('ol').insertAdAcentHTML('beforeend', option);
-}
+// Utilisez forEach avec le tableau countries
+countries.forEach(country => {
+    const option = `
+        <li class="option">
+            <div>
+                <span class="iconify" data-icon="flag:${country.code.toLowerCase()}-4x3"></span>
+                <span class="county-name">${country.name}</span>
+            </div>
+            <strong>+${country.phone}</strong>
+        </li>
+    `;
 
+    select_box.querySelector('ol').insertAdjacentHTML('beforeend', option);
+});
 
-	select_box = document.querySelector('.options'),
-	  search_box = document.querySelector('.search-box'),
-	  input_box = document.querySelector('input[type="tel"]'),
-	  selected_option = document.querySelector('.selected-option div');
-
-	  selected_option.addEventListener('click', () => {
-	  	select_box.classList.toggle('active');
-	  	selected_option.classList.toggle('active');
-	  })
+selected_option.addEventListener('click', () => {
+    select_box.classList.toggle('active');
+    selected_option.classList.toggle('active');
+});
